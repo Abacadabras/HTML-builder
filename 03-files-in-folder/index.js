ed5eb19407b842a49b1ef91f042afce87cd29c2a
Sergei Pleshanov <2357qwr@gmail.com>
2022-05-23 20:57:46 +0300

    feat: add 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const fs = require('fs/promises');
+const path = require('path');
+
+(async () => {
+  const pathFiles = path.join(__dirname, 'secret-folder');
+  const files = await fs.readdir(pathFiles, {withFileTypes: true});
+
+  files.forEach(file => {
+    if (file.isFile()) {
+      const pathFile = path.join(pathFiles, file.name);
+      const nameFile = path.parse(pathFile);
+      const statFile = async () => await fs.stat(pathFile);
+      statFile().then((data) => console.log(`${nameFile.name} - ${nameFile.ext.slice(1)} - ${data.size}b`));
+    }
+  });
+})();
